0b27557f717b1eac585a08fd6c4f3b295cd468a6
Helen Tewoldebirhan <helutb@gmail.com>
2024-08-14 10:14:04 +0300

    buy button updated

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
+const buyBtn = document.querySelector(".Buy");
+buyBtn.addEventListener('click', ()=>{
+  window.location.href = '/html/credit.html';
+});
 function showNotification(message) {
   let notification = document.createElement('div');
   notification.classList.add('notification');
